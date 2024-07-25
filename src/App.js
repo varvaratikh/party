20b3193336037ce36e5fcf249677a816dc5bfe4b
varvaratikh <tikhonovavarvara181203@gmail.com>
2024-07-25 22:13:03 +0300

    added a block of useful information

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Hello from "./pages/Hello";
 import WhoIsGenius from "./components/WhoIsGenius";
 import About from "./pages/About";
+import InfoSection from "./components/InfoSection";
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
           <Hello/>
           <WhoIsGenius/>
           <About/>
+          <InfoSection/>
         </main>
       </div>
   );
---
+++ src/components/InfoSection.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import '../styles/InfoSectionComponent.sass';
+import inf from "../images/InfoText.png";
+import star1 from "../images/star1.png";
+import star2 from "../images/star2.png";
+
+
+
+const InfoSection = () => {
+
+    return (
+        <div className='use-info'>
+            <img className='information' src={inf} alt="phrase"/>
+            <img className='star-1' src={star1} alt="star-1"/>
+            <img className='star-2' src={star2} alt="star-2"/>
+        </div>
+    );
+};
+
+export default InfoSection;
